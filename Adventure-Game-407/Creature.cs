7c49fe143948c02671f621d76cc79fcaf7e7b17d
Wie Lie Sie <wlie.sie@gmail.com>
2021-03-29 20:26:38 -0400

    updated hero, creature, and monster classes

--- Adventure-Game-407/Creature.cs
+++ Adventure-Game-407/Creature.cs
@@ -1,32 +1,24 @@
 ﻿namespace Adventure_Game_407
-{
+{   
+    //creature class - super class of subclasses hero and monster
     public class Creature
     {
-        public Weapon Weapon { get; private set; }
-        public Armor Armor { get; private set; }
-        public int Hitpoints { get; private set; }
-
-        public Creature()
-        {
-            Weapon = new Weapon();
-            Armor = new Armor();
-        }
+        public Weapon Weapon { get; protected set; }    //creature weapon
+        public Armor Armor { get; protected set; }      //creature armor
+        public int Hitpoints { get; protected set; }    //creature hit points
+        public string Name { get; protected set; }      //creature name
 
         public void Fight()
         {
             // stub
         }
-
-        public void IsAlive()
-        {
-            // stub
-        }
-
+            
         public void TakesDamage()
         {
             // stub
         }
         
+        //RestoreHealth(...) increases the amount of hit points
         public void RestoreHealth(int amount)
         {
             Hitpoints += amount;
--- Adventure-Game-407/Monster.cs
+++ Adventure-Game-407/Monster.cs
@@ -3,5 +3,23 @@
     public class Monster : Creature
     {
         private int aggression;
-    }
+        private int InventoryCapacity = 10;
+        public Dungeon CurrentDungeon { get; private set; }
+        public Room CurrentRoom { get; private set; }
+
+        //Monster constructor that takes parameter name, weapon, armor, and hitpoints
+        public Monster(string name, Weapon weapon, Armor armor, int hitpoints)
+        {
+            Name = name;
+            Weapon = weapon;
+            Armor = armor;
+            Hitpoints = hitpoints;
+        }
+
+        //IsAlive(...) returns the alive status; returns true if monster hitpoints > 0, else return false
+        public bool IsAlive()
+        {
+            return Hitpoints > 0;
+        }
+    }    
 }
