1c91dc22356d37b7d1e2f384c077d925d5c8341d
Wie Lie Sie <wlie.sie@gmail.com>
2021-03-30 11:41:26 -0400

    Added Skill.cs for experimentation purposes

--- Adventure-Game-407/Creature.cs
+++ Adventure-Game-407/Creature.cs
@@ -1,32 +1,54 @@
-﻿namespace Adventure_Game_407
+﻿using System;
+
+namespace Adventure_Game_407
 {
+    //creature class - super class of subclasses hero and monster
     public class Creature
     {
-        public Weapon Weapon { get; private set; }
-        public Armor Armor { get; private set; }
-        public int Hitpoints { get; private set; }
+        protected Weapon Weapon { get; set; }    //creature weapon
+        protected Armor Armor { get; set; }      //creature armor
+        protected int Hitpoints { get; set; }    //creature hit points
+        protected string Name { get; set; }      //creature name
+        protected Room Room { get; set; }        //creature current room location
 
-        public Creature()
+        //IsAlive(...) returns the alive status; returns true if creature hitpoints > 0, else return false
+        public bool IsAlive()
         {
-            Weapon = new Weapon();
-            Armor = new Armor();
+            return Hitpoints > 0;
         }
 
-        public void Fight()
+        //Fight method will compute the result of a fight between 2 creatures
+        //A creature will be defeated if the hitpoints dropped to 0
+        public void Fight(Creature opponent)
         {
-            // stub
+            int penetrationDamage, index;
+            while (IsAlive() && opponent.IsAlive())                                 //while loop that will run until one of creatures is not alive
+            {
+                for (index = 0; index < Weapon.GetNumAttacks(); index++)            //loop from 0 till the the max number of weapon attacks - 1
+                {
+                    penetrationDamage = StaticRandom.Instance.Next(1, 21);                      //placeholder for random number generator that generates int value between 1 and 21 (inclusive)
+                    if (damage > enemy.Armor.Strength)                                          //if opponent creature armor strength is less than the penetration number
+                    {
+                        int inflictedDamage = StaticRandom.Instance.Next(1, Weapon.GetMaxDamage);               //generate weapon damage between 1 and max weapon damage (inclusive)
+                        enemy.Hitpoints -= weaponDamage;                                                        //reduce opponent hit points by the amount of the damage inflicted
+                        Console.WriteLine(Name + " hit " + opponent.Name + " for: " + inflictedDamage + "!");   //display the current creature name, opponent name, and the amount of damage inflicted
+                    }
+                    if (enemy.IsAlive()) enemy.Fight(this);                                                     //if the opponent is still alive, then the opponent will fight back by calling Fight(...) method        
+                }
+            }
         }
 
-        public void IsAlive()
+        //Calculate the damage taken by a creature, set Hitpoints to 0 if the damage is >= creature hit points
+        public void TakesDamage(int damage)
         {
-            // stub
+            Hitpoints -= damage;
+            if (Hitpoints < 0)
+            {
+                Hitpoints = 0;
+            }
         }
 
-        public void TakesDamage()
-        {
-            // stub
-        }
-        
+        //RestoreHealth(...) increases the amount of hit points
         public void RestoreHealth(int amount)
         {
             Hitpoints += amount;
--- Adventure-Game-407/MagicalMonster.cs
+++ Adventure-Game-407/MagicalMonster.cs
@@ -1,7 +1,18 @@
 ﻿namespace Adventure_Game_407
 {
-    public class MagicalMonster
+    public class MagicalMonster : Creature
     {
-        
+        private readonly int Aggression;
+        private List<Skill> MagicSkills { get; set; }
+
+        //MagicalMonster constructor that takes parameter name, weapon, armor, hitpoints, agression level, and monster type
+        public MagicalMonster(string name, Weapon weapon, Armor armor, int hitpoints, int aggression)
+        {
+            Name = name;
+            Weapon = weapon;
+            Armor = armor;
+            Hitpoints = hitpoints;
+            Aggression = aggression;           
+        }
     }
 }
