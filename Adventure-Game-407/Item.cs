f241f9f574dffb13abf054c0f3c8fa078e0e1f6b
Shawn Huesman <50417810+mrshawnhuesman@users.noreply.github.com>
2021-04-02 00:15:02 -0400

    Added Item Drop into Room even if not Hero and Hero now Removes Item From Inventory Upon Item Drop

--- Adventure-Game-407/Item.cs
+++ Adventure-Game-407/Item.cs
@@ -13,13 +13,17 @@ namespace Adventure_Game_407
         {
             Console.WriteLine("This item is not usable.");
         }
-        
+
         public void Drop()
         {
             if (Owner is Hero)
             {
+                // remove item from inventory
                 ((Hero) Owner).Inventory.Remove(this);
             }
+            
+            // drop item into room
+            RoomOccupied = ((Hero) Owner).Room;
         }
     }
 }
